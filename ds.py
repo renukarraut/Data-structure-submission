77151928e9a92f8480fc30c072d53daa5791e038
renukarraut <102784199+renukarraut@users.noreply.github.com>
2022-12-27 20:51:05 +0530

    Create ds.py

---
+++ ds.py
@@ -0,0 +1,8 @@
+#Write a program to find the smallest number using a stack.
+arr = [111, 13, 25, 9, 34, 1]
+n = len(arr)
+
+arr.sort()
+
+print("smallest element is "+str(arr[0]))
+print("second smallest element is "+str(arr[1]))
